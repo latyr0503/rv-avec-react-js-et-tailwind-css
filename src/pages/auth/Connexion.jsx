044b4353ac15d9b658f2c99b39ee3967353ff401
latyr0503 <latyr0503@gmail.com>
2024-07-30 02:36:27 +0000

    ajout api auth

--- src/pages/auth/Connexion.jsx
+++ src/pages/auth/Connexion.jsx
@@ -1,48 +1,93 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
 import { SlSocialGoogle } from "react-icons/sl";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Connexion = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.username || !formData.password) {
+      alert("Tous les champs doivent être remplis.");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/auth/signin",
+        formData
+      );
+      console.log(response.data);
+      navigate("/dashboard");
+      alert("Connexion réussie");
+    } catch (error) {
+      if (error.response) {
+        setErrorMessage(
+          error.response.data.message || "Une erreur s'est produite"
+        );
+        alert("Verifier les informations saisies");
+      } else {
+        setErrorMessage("Une erreur s'est produite");
+        alert("Une erreur s'est produite");
+      }
+    }
+  };
   return (
-    <div >
+    <div>
       <h2 className="text-center text-3xl font-semibold">Se Connecter</h2>
       <div className="grid grid-cols-3 gap-4  overflow-hidden">
-        
         <div className="relative ">
           <div className="w-52 -rotate-45 h-80 rounded-2xl absolute top-8 -left-28 bg-sky-200"></div>
           <div className="w-52 rotate-12 h-80 rounded-2xl absolute top-8 -left-40 bg-sky-500"></div>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
-            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mt-8 mb-5 "
-            type="email"
-            placeholder="Entrer votre mail"
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 my-5"
+            type="text"
+            name="username"
+            placeholder="Entrer votre username"
+            onChange={handleChange}
+            value={formData.username}
           />
           <input
-            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5 "
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5"
             type="password"
-            placeholder="Entrer votre mot de passe"
+            name="password"
+            placeholder="Entrer votre password"
+            onChange={handleChange}
+            value={formData.password}
           />
-          <Link to="/dashboard">
-            <button
-              type="button"
-              className="w-full my-8 py-3 text-white duration-150 bg-sky-500 rounded-lg hover:bg-sky-700 active:shadow-lg"
-            >
-              Connexion
-            </button>
-          </Link>
+          <button
+            type="submit"
+            className="w-full my-8 py-3 text-white duration-150 bg-sky-500 rounded-lg hover:bg-sky-700 active:shadow-lg"
+          >
+            Connexion
+          </button>
           <button className="w-full py-3 flex justify-center items-center text-white duration-150 bg-sky-900 rounded-lg hover:bg-black active:shadow-lg">
             <span className="me-2">Continuer avec Google</span>
             <SlSocialGoogle />
           </button>
           <p className="flex justify-center items-center my-10">
-            <span>Mot de passe oublier ?</span>
-            <span>S'inscrire</span>
+            <span>Mot de passe oublier ? </span>
+            <Link to="/inscription">
+              <span className="text-sky-700 font-bold mx-5"> S'inscrire</span>
+            </Link>
           </p>
         </form>
-
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <div className="relative">
           <div className="w-48 -rotate-45 h-48 rounded-2xl absolute bottom-24 -right-12 bg-sky-200"></div>
           <div className="w-48 rotate-12 h-48 rounded-2xl absolute bottom-36 -right-10 bg-sky-500"></div>
--- src/pages/auth/Inscription.jsx
+++ src/pages/auth/Inscription.jsx
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { SlSocialGoogle } from "react-icons/sl";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Inscription = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    address: "",
+    speciality: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      !formData.username ||
+      !formData.fullName ||
+      !formData.email ||
+      !formData.phoneNumber ||
+      !formData.password ||
+      !formData.address ||
+      !formData.speciality
+    ) {
+      alert("Tous les champs doivent être remplis.");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/auth/signup",
+        formData
+      );
+      console.log(response.data);
+      navigate("/dashboard");
+      alert("Inscription reussie");
+    } catch (error) {
+      if (error.response) {
+        // Gérer les erreurs du backend
+        if (error.response.status === 409) {
+          // Erreur 409 : Conflit, par exemple, username ou email déjà pris
+          alert("Le nom d'utilisateur ou l'email est déjà pris.");
+        } else {
+          setErrorMessage(
+            error.response.data.message || "Une erreur s'est produite"
+          );
+        }
+      } else {
+        setErrorMessage("Une erreur s'est produite");
+      }
+    }
+  };
+
   return (
     <div>
       <h2 className="text-center text-3xl font-semibold">S'inscrire</h2>
@@ -11,37 +68,77 @@ export const Inscription = () => {
           <div className="w-56 -rotate-45 h-72 rounded-2xl absolute top-20 -left-24 bg-sky-200"></div>
           <div className="w-56 rotate-12 h-80 rounded-2xl absolute top-28 -left-40 bg-sky-500"></div>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
-            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 my-5 "
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 my-5"
             type="text"
-            placeholder="Entrer votre nom"
+            name="username"
+            placeholder="Entrer votre username"
+            onChange={handleChange}
+            value={formData.username}
           />
           <input
-            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5 "
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5"
             type="text"
-            placeholder="Entrer votre prenom"
+            name="fullName"
+            placeholder="Entrer votre fullName"
+            onChange={handleChange}
+            value={formData.fullName}
           />
           <input
-            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5 "
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5"
             type="email"
-            placeholder="Entrer votre mail"
+            name="email"
+            placeholder="Entrer votre email"
+            onChange={handleChange}
+            value={formData.email}
           />
           <input
-            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5 "
-            type="password"
-            placeholder="Entrer votre mot de passe"
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5"
+            type="number"
+            name="phoneNumber"
+            placeholder="Entrer votre phoneNumber"
+            onChange={handleChange}
+            value={formData.phoneNumber}
           />
-          <Link to="/dashboard">
-            <button className="w-full my-8 py-3 text-white duration-150 bg-sky-500 rounded-lg hover:bg-sky-700 active:shadow-lg">
-              S'inscrire
-            </button>
-          </Link>
-          <button className="w-full py-3 flex justify-center items-center text-white duration-150 bg-sky-900 rounded-lg hover:bg-black active:shadow-lg">
-            <span className="me-2">Continuer avec Google</span>
-            <SlSocialGoogle />
+          <input
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5"
+            type="password"
+            name="password"
+            placeholder="Entrer votre password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+          <input
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5"
+            type="text"
+            name="address"
+            placeholder="Entrer votre address"
+            onChange={handleChange}
+            value={formData.address}
+          />
+          <input
+            className="bg-gray-200 border-none w-full h-12 rounded-lg px-4 mb-5"
+            type="text"
+            name="speciality"
+            placeholder="Entrer votre speciality"
+            onChange={handleChange}
+            value={formData.speciality}
+          />
+          <button
+            type="submit"
+            className="w-full py-3 text-white duration-150 bg-sky-500 rounded-lg hover:bg-sky-700 active:shadow-lg"
+          >
+            S'inscrire
           </button>
+          <p className="flex justify-center items-center my-10">
+            <span>J'ai deja un compte ! </span>
+            <Link to="/connexion">
+              <span className="text-sky-700 font-bold mx-5"> Se Connecter</span>
+            </Link>
+          </p>
         </form>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <div className="relative py-10">
           <div className="w-48 -rotate-45 h-48 rounded-2xl absolute bottom-12 -right-1 bg-sky-200"></div>
           <div className="w-48 rotate-12 h-48 rounded-2xl absolute bottom-10 -right-16 bg-sky-500"></div>
